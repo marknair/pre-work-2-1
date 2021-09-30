2fa04cbf8bb3546b6aad245cc936358b1658a601
Mark Nair <mark@babblebox.com>
2021-09-29 21:31:20 -0500

    Some JS

---
+++ conditionals-functions-scope.js
@@ -0,0 +1,27 @@
+/********************************************
+ * Conditionals, Functions, Scope, and Loops
+ ********************************************/
+
+// let equals = 1 === 1;
+// console.log(equals)
+
+// let storeA = 4.40
+// let storeB = 4.40
+//
+// let isItLower = storeA < storeB
+// console.log(isItLower)
+//
+// if (isItLower) {
+//     console.log("Store A has the lower price.")
+// } else if (storeB < storeA) {
+//     console.log("Store B is the boss.")
+// } else {
+//     console.log("The prices are equal.")
+// }
+
+function squareThisNumber(number) {
+    return number * number
+}
+
+let squaredNumber = squareThisNumber(7)
+console.log(squaredNumber)
